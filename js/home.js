7632bea77d4ba84f8f1db73c36b60bc2a05d2645
Danel Yehuda“ <danelyehuda1998@gmail.com>
2024-07-30 13:14:03 +0300

    feat: Update kid profile picture and display savings amount on home page
    This commit modifies the home.js file to update the kid profile picture and display the updated savings amount on the home page. It fetches the updated kid data from the server using the API endpoint and stores it in the session storage. The profile picture element is then updated with the new image URL, and the savings amount element is updated with the updated kid's savings amount. This change enhances the visual presentation and user experience of the home page, providing the latest information for the kid's profile.

--- js/home.js
+++ js/home.js
@@ -19,12 +19,19 @@ document.addEventListener('DOMContentLoaded', function() {
         fetchMessages(user.data.user_id, 'user'); // Fetch messages for parent
     }
     if (kid) {
-        profilePicElement.src = "images/kid1.jpg";
-        document.getElementById('kidsNav').style.display = 'none';
-        document.getElementById('tasksNav').style.display = 'none';
-        kidSavingsElement.style.display = 'block';
-        savingsAmountElement.textContent = kid.data.kid_coins;
-        fetchMessages(kid.data.kid_id, 'kid'); // Fetch messages for kid
+        fetch(`http://localhost:8080/api/kids/${kid.data.kid_id}`)
+        .then(response => response.json())
+        .then(updatedKid => {
+            console.log('Updated kid data:', updatedKid);
+            sessionStorage.setItem('kid', JSON.stringify(updatedKid));
+            profilePicElement.src = "images/kid1.jpg";
+            document.getElementById('kidsNav').style.display = 'none';
+            document.getElementById('tasksNav').style.display = 'none';
+            kidSavingsElement.style.display = 'block';
+            savingsAmountElement.textContent = updatedKid.data.kid_coins;
+            fetchMessages(updatedKid.data.kid_id, 'kid'); // Fetch messages for kid
+        })
+        .catch(error => console.error('Error fetching updated kid data:', error));
     }
 
     // Fetch and display publish tasks
